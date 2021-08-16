babfd4773ad02d3f2551ff2e5fdae833a0801ff3
grze2000 <programista333@gmail.com>
2021-08-16 16:42:46 +0200

    initial commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView  style={styles.container}>
+      <Text>home</Text>
+    </SafeAreaView>
+  )
+}
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+  
